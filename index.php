be442b82b06493ba1bdbcb0e04ef5807ddbd744c
Mark Fabrizio <fabrizim@owlwatch.com>
2012-03-03 12:50:09 -0500

    Make a proper wordpress plugin

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-
+/*
+Plugin Name: Snap Plugin
+Plugin URI: https://github.com/fabrizim/Snap
+Description: PHP Snap Library for Wordpress. Snap on some functionality!
+Author: Mark Fabrizio
+Version: 1.0
+Author URI: http://owlwatch.com/
+*/
 require(dirname(__FILE__).'/src/Snap.php');
