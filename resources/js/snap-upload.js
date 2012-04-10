aaf689daacd8f2b84fa9167e87e388f908440db2
Mark Fabrizio <fabrizim@owlwatch.com>
2012-04-10 08:10:18 -0400

    Remove console.log

--- resources/js/snap-upload.js
+++ resources/js/snap-upload.js
@@ -56,7 +56,6 @@
     
     function snap_send_to_editor(arg, self)
     {
-        console.log(arguments);
         self.attr('data-use_id') ? save_json(arg, self) : save_html(arg, self);
         tb_remove();
         return false;
